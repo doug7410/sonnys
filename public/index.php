36fadf12ec097dfc8e53d21729a0f90d87e01aff
Doug Steinberg <dsteinberg@advantone.com>
2017-06-11 03:42:23 -0400

    added header for cors

--- public/index.php
+++ public/index.php
@@ -6,21 +6,24 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Laravel\Lumen\Application();
 
+
 $app->post('/login', function (\Illuminate\Http\Request $request){
+    header("Access-Control-Allow-Origin: *");
     $username = $request->input('username');
     $password = $request->input('password');
 
-    if ($username == 'username' && $password == 'password') {
-        return JsonResponse::create([
-            'success' =>
-                ['message' => 'successful login']
-        ]);
+    if ($username == 'u' && $password == 'p') {
+         return JsonResponse::create([
+             'success' =>
+                 ['message' => 'successful login']
+         ]);
     } else {
-        return JsonResponse::create([
-            'error' =>
-                ['message' => 'invalid username or password']
-        ], 422);
+         return JsonResponse::create([
+             'error' =>
+                 ['message' => 'invalid username or password']
+         ], 500);
     }
+
 });
 
 $app->run();
